03d30e2f039cac96bfeff38eea364744062cae2e
j3rrr <bambule96@gmail.com>
2019-03-03 14:00:08 +0100

    comment form validation & admin nav personal

--- functions.php
+++ functions.php
@@ -94,18 +94,24 @@ function createComment()
         $comment_date    = date('d-m-y');
         $comment_status  = "unapproved";
 
-        $query = "INSERT INTO comments(comment_post_id,comment_author,comment_email,comment_content,comment_status,comment_date) ";
-        $query .= "VALUES({$post_id},'{$comment_author}','{$comment_email}','{$comment_content}','{$comment_status}',now()) ";
+        if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
+            $query = "INSERT INTO comments(comment_post_id,comment_author,comment_email,comment_content,comment_status,comment_date) ";
+            $query .= "VALUES({$post_id},'{$comment_author}','{$comment_email}','{$comment_content}','{$comment_status}',now()) ";
 
-        $create_comment_query = mysqli_query($connection, $query);
-        if ($create_comment_query) {
-            echo "<div class='alert alert-success'>Successfully created Comment. Please wait for approval.</div>";
+            $create_comment_query = mysqli_query($connection, $query);
+            if ($create_comment_query) {
+                echo "<div class='alert alert-success'>Successfully created Comment. Please wait for approval.</div>";
+            }
+            queryFailed($create_comment_query);
+
+            $count_query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = {$post_id}";
+            $comment_count_query = mysqli_query($connection, $count_query);
+            queryFailed($comment_count_query);
+        } else {
+            echo "<script>
+            alert('Fields can not be empty');
+            </script>";
         }
-        queryFailed($create_comment_query);
-
-        $count_query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = {$post_id}";
-        $comment_count_query = mysqli_query($connection, $count_query);
-        queryFailed($comment_count_query);
     }
 }
 
